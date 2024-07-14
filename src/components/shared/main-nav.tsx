b88599ece684ed76662bc9a63078e61b5844dbe6
Muhammad Sholeh <avsholeh@gmail.com>
2024-07-14 07:13:50 +0700

    feat: improvement

--- src/components/shared/main-nav.tsx
+++ src/components/shared/main-nav.tsx
@@ -1,4 +1,4 @@
-import { CircleUser, Menu, Package2 } from "lucide-react";
+import { CircleUser, LibraryBig, Menu, Package2 } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
@@ -14,27 +14,25 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import ToggleTheme from "./toggle-theme";
 
 export default function MainNav() {
+    const isLoggedIn = false; // Replace with auth logic
     return (
-        <header className="sticky top-0 flex h-16 w-full items-center gap-4 border-b bg-background px-4 md:px-6">
+        <header className="px-content sticky top-0 flex h-16 w-full items-center justify-between gap-4 border-b bg-background">
             <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
                 <Link href="#" className="flex items-center gap-2 text-lg font-semibold md:text-base">
-                    <Package2 className="h-6 w-6" />
-                    <span className="sr-only">Acme Inc</span>
+                    <LibraryBig className="mr-1 h-6 w-6" /> ResumeFabric
+                    <span className="sr-only">ResumeFabric</span>
                 </Link>
                 <Link href="#" className="text-muted-foreground transition-colors hover:text-foreground">
-                    Dashboard
+                    Resume Builder
                 </Link>
                 <Link href="#" className="text-muted-foreground transition-colors hover:text-foreground">
-                    Orders
+                    Resume Examples
                 </Link>
                 <Link href="#" className="text-muted-foreground transition-colors hover:text-foreground">
-                    Products
+                    Cover Letter Builder
                 </Link>
                 <Link href="#" className="text-muted-foreground transition-colors hover:text-foreground">
-                    Customers
-                </Link>
-                <Link href="#" className="text-foreground transition-colors hover:text-foreground">
-                    Settings
+                    Cover Letter Examples
                 </Link>
             </nav>
             <Sheet>
@@ -48,45 +46,51 @@ export default function MainNav() {
                     <nav className="grid gap-6 text-lg font-medium">
                         <Link href="#" className="flex items-center gap-2 text-lg font-semibold">
                             <Package2 className="h-6 w-6" />
-                            <span className="sr-only">Acme Inc</span>
+                            <span className="sr-only">ResumeFabric</span>
                         </Link>
                         <Link href="#" className="text-muted-foreground hover:text-foreground">
-                            Dashboard
+                            Resume Builder
                         </Link>
                         <Link href="#" className="text-muted-foreground hover:text-foreground">
-                            Orders
+                            Resume Examples
                         </Link>
                         <Link href="#" className="text-muted-foreground hover:text-foreground">
-                            Products
+                            Cover Letter Builder
                         </Link>
                         <Link href="#" className="text-muted-foreground hover:text-foreground">
-                            Customers
-                        </Link>
-                        <Link href="#" className="hover:text-foreground">
-                            Settings
+                            Cover Letter Examples
                         </Link>
                     </nav>
                 </SheetContent>
             </Sheet>
-            <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
-                <ToggleTheme />
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="secondary" size="icon" className="rounded-full">
-                            <CircleUser className="h-5 w-5" />
-                            <span className="sr-only">Toggle user menu</span>
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Settings</DropdownMenuItem>
-                        <DropdownMenuItem>Support</DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Logout</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
+            {isLoggedIn ? (
+                <div className="flex items-center justify-end gap-4 md:gap-2 lg:gap-4">
+                    <ToggleTheme />
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="secondary" size="icon" className="rounded-full">
+                                <CircleUser className="h-5 w-5" />
+                                <span className="sr-only">Toggle user menu</span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Settings</DropdownMenuItem>
+                            <DropdownMenuItem>Support</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Logout</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
+            ) : (
+                <div className="flex items-center justify-end gap-4 md:gap-2 lg:gap-4">
+                    <Button className="border border-primary bg-background text-primary hover:bg-primary hover:text-primary-foreground">
+                        Build My Resume
+                    </Button>
+                    <ToggleTheme />
+                </div>
+            )}
         </header>
     );
 }
