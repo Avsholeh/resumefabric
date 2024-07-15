fcd8b73e70374cbf3bb5ec8af7b7a4fc3ea58029
Muhammad Sholeh <avsholeh@gmail.com>
2024-07-15 21:03:00 +0700

    feat: add date picker

---
+++ src/components/shared/date-picker.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import { Calendar } from "../ui/calendar";
+import { FormControl } from "../ui/form";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+
+type DateSelectorProps = {
+    field: {
+        value: string | null;
+        onChange: (date: string | undefined) => void;
+    };
+}
+
+export default function DatePicker({ field }: DateSelectorProps) {
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <FormControl>
+                    <Button
+                        variant={"outline"}
+                        className={cn("w-full pl-3 text-left font-normal", !field.value && "text-muted-foreground")}
+                    >
+                        {!!field.value ? field.value : <span>Pick a date</span>}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                    mode="single"
+                    onSelect={(date) => field.onChange(date ? format(date, "PPP") : undefined)}
+                    disabled={(date) => date > new Date() || date < new Date("1900-01-01")}
+                    initialFocus
+                />
+            </PopoverContent>
+        </Popover>
+    );
+}
