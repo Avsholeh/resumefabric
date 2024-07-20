86b5787b53c0456fe9324416f14de82c055a8faa
Muhammad Sholeh <avsholeh@gmail.com>
2024-07-20 15:05:37 +0700

    feat: add webvital reports

--- src/app/(resume)/layout.tsx
+++ src/app/(resume)/layout.tsx
@@ -1,10 +1,25 @@
 import AppNav from "@/components/shared/app-nav";
+import WebvitalProvider from "@/components/webvital-provider";
 
 export default function ResumeLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    /**
+     * Webvital is a custom hook that reports web vitals to the console.
+     * If the environment is development, wrap the children with the WebvitalProvider.
+     * Otherwise, render the children without the WebvitalProvider.
+     */
+    if ((process.env.NODE_ENV as string) === "development") {
+        return (
+            <WebvitalProvider>
+                <AppNav />
+                <div className="px-app flex flex-col items-center">{children}</div>
+            </WebvitalProvider>
+        );
+    }
+
     return (
         <>
             <AppNav />
---
+++ src/components/webvital-provider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useReportWebVitals } from "next/web-vitals";
+
+export default function WebvitalProvider({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    useReportWebVitals((metric) => {
+        console.log(metric);
+    });
+    return <>{children}</>;
+}
