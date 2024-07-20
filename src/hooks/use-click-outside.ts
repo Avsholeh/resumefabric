0b780959065e317f272bc420b4495c17a9825729
Muhammad Sholeh <avsholeh@gmail.com>
2024-07-20 13:49:39 +0700

    chore: update use click outside hook

--- src/hooks/use-click-outside.ts
+++ src/hooks/use-click-outside.ts
@@ -1,21 +1,17 @@
 import { MutableRefObject, useEffect } from "react";
 
-export function useClickOutside(ref: MutableRefObject<null>, callback: () => void) {
-    useEffect(
-        function () {
-            const listener = (event: MouseEvent | TouchEvent) => {
-                if (!ref.current || (ref.current as any).contains(event.target)) {
-                    return;
-                }
+export function useClickOutside(ref: MutableRefObject<null | HTMLElement>, callback: () => void) {
+    useEffect(() => {
+        const listener = (event: MouseEvent | TouchEvent) => {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
                 callback();
-            };
-            document.addEventListener("mousedown", listener);
-            document.addEventListener("touchstart", listener);
-            return function () {
-                document.removeEventListener("mousedown", listener);
-                document.removeEventListener("touchstart", listener);
-            };
-        },
-        [ref, callback]
-    );
+            }
+        };
+        document.addEventListener("mousedown", listener);
+        document.addEventListener("touchstart", listener);
+        return function () {
+            document.removeEventListener("mousedown", listener);
+            document.removeEventListener("touchstart", listener);
+        };
+    }, [ref, callback]);
 }
