23198591d7c5b202e0c6d443b2527b2b9f3ac1fc
Muhammad Sholeh <avsholeh@gmail.com>
2024-07-20 20:13:54 +0700

    chore: rename additional dir

--- src/app/(resume)/additional/page.tsx
+++ src/app/(resume)/additional/page.tsx
@@ -1,5 +1,5 @@
 import ResumeContainer from "@/components/shared/resume-container";
-import AdditionalForm from "@/features/resume/addtional/components/form";
+import AdditionalForm from "@/features/resume/additional/components/form";
 import ClassicTemplate from "@/features/resume/templates/classic";
 
 export default function AdditionalPage() {
