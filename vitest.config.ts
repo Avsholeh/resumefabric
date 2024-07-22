41d2c2436eb6f2d43449f0b08a0398fd73871927
Muhammad Sholeh <avsholeh@gmail.com>
2024-07-22 20:39:17 +0700

    chore: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,8 +5,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     plugins: [tsconfigPaths(), react()],
     test: {
-        environment: "jsdom",
-        setupFiles: ["vitest-localstorage-mock"],
-        mockReset: false,
+        environment: "jsdom"
     },
 });
