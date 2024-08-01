1d9f4abf896c719c93cb958614808f455c2f75e5
Muhammad Sholeh <avsholeh@gmail.com>
2024-08-01 17:42:29 +0700

    fix: skill page and form

--- src/app/(resume)/skills/page.tsx
+++ src/app/(resume)/skills/page.tsx
@@ -1,11 +1,35 @@
-import ClassicTemplate from "@/components/resume/templates/classic";
+"use client";
+
 import ResumeContainer from "@/components/shared/resume-container";
+import { Form } from "@/components/ui/form";
+import { SkillSchema, SkillType } from "@/schema/skills";
+import { SkillsDefault } from "@/store/resume/default";
+import { useResumeStore } from "@/store/resume/provider";
+import { zodResolver } from "@hookform/resolvers/zod";
 import dynamic from "next/dynamic";
+import { useForm } from "react-hook-form";
 
 const SkillsFormDynamic = dynamic(() => import("@/components/resume/skills/form"), {
   ssr: false, // Disable Server Side Rendering
 });
 
+const ClassicTemplateDynamic = dynamic(() => import("@/components/resume/templates/classic"), {
+  ssr: false, // Disable server side rendering for this component
+});
+
 export default function SkillPage() {
-  return <ResumeContainer form={<SkillsFormDynamic />} template={<ClassicTemplate />} />;
+  const { getResumeItem } = useResumeStore((state) => state);
+
+  // Create a form with the useForm hook
+  // https://react-hook-form.com/api/useform
+  const form = useForm<SkillType>({
+    resolver: zodResolver(SkillSchema),
+    defaultValues: getResumeItem("skills") ?? SkillsDefault,
+  });
+
+  return (
+    <Form {...form}>
+      <ResumeContainer form={<SkillsFormDynamic />} template={<ClassicTemplateDynamic />} />
+    </Form>
+  );
 }
--- src/components/resume/skills/form.tsx
+++ src/components/resume/skills/form.tsx
@@ -4,26 +4,21 @@ import FormButtonGroup from "@/components/shared/form-button-group";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
-import { SkillSchema, type SkillType } from "@/schema/skills";
-import { SkillsDefault } from "@/store/resume/default";
+import { FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { type SkillType } from "@/schema/skills";
 import { useResumeStore } from "@/store/resume/provider";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { SubmitHandler, useFieldArray, useForm } from "react-hook-form";
+import { SubmitHandler, useFieldArray, useFormContext } from "react-hook-form";
 import SkillCard from "./skill-card";
 
 export default function SkillsForm(): React.ReactElement {
   const router = useRouter();
 
-  const { getResumeItem, updateResumeItem } = useResumeStore((state) => state);
+  const { updateResumeItem } = useResumeStore((state) => state);
 
   // Create a form with the useForm hook
   // https://react-hook-form.com/api/useform
-  const form = useForm<SkillType>({
-    resolver: zodResolver(SkillSchema),
-    defaultValues: getResumeItem("skills") ?? SkillsDefault,
-  });
+  const form = useFormContext<SkillType>();
 
   // Use the useFieldArray hook to manage the skills array
   // https://react-hook-form.com/api/usefieldarray
@@ -39,51 +34,49 @@ export default function SkillsForm(): React.ReactElement {
   };
 
   return (
-    <Form {...form}>
-      <form id="skills-form" onSubmit={form.handleSubmit(onSubmit)}>
-        <FormButtonGroup nextURL="/additional" prevURL="/education" showSkip isLoading={form.formState.isSubmitting} />
+    <form id="skills-form" onSubmit={form.handleSubmit(onSubmit)}>
+      <FormButtonGroup nextURL="/additional" prevURL="/education" showSkip isLoading={form.formState.isSubmitting} />
 
-        <Card className="mb-20">
-          <CardHeader>
-            <CardTitle>Skills</CardTitle>
-            <CardDescription>List your key skills and areas of expertise.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="mb-5 flex">
-              <FormField
-                name="showExperienceLevel"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem className="flex items-center space-x-2">
-                    <FormControl>
-                      <Checkbox checked={field.value} onCheckedChange={field.onChange} className="mt-2" />
-                    </FormControl>
-                    <FormLabel>Show experience level</FormLabel>
-                  </FormItem>
-                )}
-              />
-            </div>
+      <Card className="mb-20">
+        <CardHeader>
+          <CardTitle>Skills</CardTitle>
+          <CardDescription>List your key skills and areas of expertise.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="mb-5 flex">
+            <FormField
+              name="showExperienceLevel"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="flex items-center space-x-2">
+                  <FormControl>
+                    <Checkbox checked={field.value} onCheckedChange={field.onChange} className="mt-2" />
+                  </FormControl>
+                  <FormLabel>Show experience level</FormLabel>
+                </FormItem>
+              )}
+            />
+          </div>
 
-            {fields.map((skill, index) => (
-              <SkillCard
-                key={skill.id}
-                skill={skill}
-                index={index}
-                control={form.control}
-                watch={form.watch}
-                remove={remove}
-                isDelete={fields.length > 1}
-              />
-            ))}
+          {fields.map((skill, index) => (
+            <SkillCard
+              key={skill.id}
+              skill={skill}
+              index={index}
+              control={form.control}
+              watch={form.watch}
+              remove={remove}
+              isDelete={fields.length > 1}
+            />
+          ))}
 
-            <div className="mt-3 flex w-full justify-end">
-              <Button type="button" variant={"ghost"} onClick={() => append({ name: "", experienceLevel: 3 })}>
-                + Add more skills
-              </Button>
-            </div>
-          </CardContent>
-        </Card>
-      </form>
-    </Form>
+          <div className="mt-3 flex w-full justify-end">
+            <Button type="button" variant={"ghost"} onClick={() => append({ name: "", experienceLevel: 3 })}>
+              + Add more skills
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </form>
   );
 }
