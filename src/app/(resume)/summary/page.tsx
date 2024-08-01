534f004093cfdd5312f83e5a71b14ce980fd0246
Muhammad Sholeh <avsholeh@gmail.com>
2024-08-01 18:54:12 +0700

    fix: summary page and form

--- src/app/(resume)/summary/page.tsx
+++ src/app/(resume)/summary/page.tsx
@@ -1,7 +1,33 @@
-import SummaryForm from "@/components/resume/summary/form";
-import ClassicTemplate from "@/components/resume/templates/classic";
+"use client";
+
 import ResumeContainer from "@/components/shared/resume-container";
+import { Form } from "@/components/ui/form";
+import { SummarySchema } from "@/schema/summary";
+import { SummaryDefault } from "@/store/resume/default";
+import { useResumeStore } from "@/store/resume/provider";
+import { zodResolver } from "@hookform/resolvers/zod";
+import dynamic from "next/dynamic";
+import { useForm } from "react-hook-form";
+
+const SummaryFormDynamic = dynamic(() => import("@/components/resume/summary/form"), {
+  ssr: false, // Disable server side rendering for this component
+});
+
+const ClassicTemplateDynamic = dynamic(() => import("@/components/resume/templates/classic"), {
+  ssr: false, // Disable server side rendering for this component
+});
 
 export default function SummaryPage() {
-  return <ResumeContainer form={<SummaryForm />} template={<ClassicTemplate />} />;
+  const { getResumeItem } = useResumeStore((state) => state);
+
+  const form = useForm({
+    resolver: zodResolver(SummarySchema),
+    defaultValues: getResumeItem("summary") ?? SummaryDefault,
+  });
+
+  return (
+    <Form {...form}>
+      <ResumeContainer form={<SummaryFormDynamic />} template={<ClassicTemplateDynamic />} />
+    </Form>
+  );
 }
--- src/components/resume/summary/form.tsx
+++ src/components/resume/summary/form.tsx
@@ -5,111 +5,111 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { SummaryDefaultValues, SummaryField, SummarySchema } from "@/schema/summary";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { SummaryType } from "@/schema/summary";
 import { useRouter } from "next/navigation";
 import { useRef } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useFormContext } from "react-hook-form";
 
 export default function SummaryForm(): React.ReactElement {
   const router = useRouter();
-  const fileUploadRef = useRef<HTMLInputElement>(null);
-  const form = useForm({
-    resolver: zodResolver(SummarySchema),
-    defaultValues: SummaryDefaultValues,
-  });
 
-  const onSubmit: SubmitHandler<SummaryField> = (fieldValue) => {
+  // This hook is used to create a reference to the file upload input element
+  const fileUploadRef = useRef<HTMLInputElement>(null);
+
+  // This hook is used to create a form instance with the useForm hook
+  // https://react-hook-form.com/api/useform
+  const form = useFormContext<SummaryType>();
+
+  const onSubmit: SubmitHandler<SummaryType> = (fieldValue) => {
     console.log(fieldValue);
-    router.push("/final");
+    // router.push("/final");
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} id="summary-form">
-        <FormButtonGroup nextURL="/final" prevURL="/additional" showSkip />
+    <form onSubmit={form.handleSubmit(onSubmit)} id="summary-form">
+      <FormButtonGroup nextURL="/final" prevURL="/additional" showSkip />
 
-        <Card>
-          <CardHeader>
-            <CardTitle>Summary</CardTitle>
-            <CardDescription>
-              Provide a brief summary of your professional background and career objectives.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="mb-2 mt-3 flex justify-between">
-              <span>Profile Picture</span>
-              <FormField
-                name="hideProfilePicture"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem className="flex items-center space-x-2">
-                    <FormControl>
-                      <Checkbox onCheckedChange={field.onChange} checked={field.value} className="mt-2" />
-                    </FormControl>
-                    <FormLabel>Hide Profile Picture</FormLabel>
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <div className="mb-5 flex items-center justify-between">
-              <Avatar className="h-14 w-14">
-                <AvatarImage src="" alt="profile-picture" />
-                <AvatarFallback>RF</AvatarFallback>
-              </Avatar>
-              <div className="flex flex-col">
-                <FormField
-                  name="profilePicture"
-                  control={form.control}
-                  render={({ field }) => (
-                    <FormItem>
-                      <Button
-                        type="button"
-                        variant={"outline"}
-                        onClick={() => {
-                          fileUploadRef.current?.click();
-                        }}
-                      >
-                        Choose File
-                      </Button>
-                      <FormControl className="hidden">
-                        <Input
-                          {...field}
-                          type="file"
-                          ref={fileUploadRef}
-                          accept="image/jpeg, image/png"
-                          onChange={(e) => field.onChange(e.target.files ? e.target.files[0] : null)}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-
-                {form.formState.errors.profilePicture && (
-                  <span className="text-error mt-2 text-sm">{form.formState.errors.profilePicture.message}</span>
-                )}
-              </div>
-            </div>
-
+      <Card>
+        <CardHeader>
+          <CardTitle>Summary</CardTitle>
+          <CardDescription>
+            Provide a brief summary of your professional background and career objectives.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="mb-2 mt-3 flex justify-between">
+            <span>Profile Picture</span>
             <FormField
-              name="summary"
+              name="hideProfilePicture"
               control={form.control}
               render={({ field }) => (
-                <FormItem className="mb-2 w-full">
-                  <FormLabel>Description</FormLabel>
+                <FormItem className="flex items-center space-x-2">
                   <FormControl>
-                    <Textarea {...field} />
+                    <Checkbox onCheckedChange={field.onChange} checked={field.value} className="mt-2" />
                   </FormControl>
+                  <FormLabel>Hide Profile Picture</FormLabel>
                 </FormItem>
               )}
             />
-          </CardContent>
-        </Card>
-      </form>
-    </Form>
+          </div>
+
+          <div className="mb-5 flex items-center justify-between">
+            <Avatar className="h-14 w-14">
+              <AvatarImage src="" alt="profile-picture" />
+              <AvatarFallback>RF</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <FormField
+                name="profilePicture"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <Button
+                      type="button"
+                      variant={"outline"}
+                      onClick={() => {
+                        fileUploadRef.current?.click();
+                      }}
+                    >
+                      {field.value ? "Change" : "Upload"}
+                    </Button>
+                    <FormControl className="hidden">
+                      <Input
+                        {...field}
+                        type="file"
+                        ref={fileUploadRef}
+                        accept="image/jpeg, image/png"
+                        onChange={(e) => field.onChange(e.target.files ? e.target.files[0] : undefined)}
+                        value={""}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              {form.formState.errors.profilePicture && (
+                <span className="text-error mt-2 text-sm">{form.formState.errors.profilePicture.message}</span>
+              )}
+            </div>
+          </div>
+
+          <FormField
+            name="summary"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="mb-2 w-full">
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </CardContent>
+      </Card>
+    </form>
   );
 }
