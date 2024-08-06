2c57e0d578d8f0755bd8746ae1323c62dcb32641
Muhammad Sholeh <avsholeh@gmail.com>
2024-08-06 11:34:44 +0700

    chore: improve classic template

--- src/components/resume/templates/classic.tsx
+++ src/components/resume/templates/classic.tsx
@@ -16,7 +16,7 @@ export default function ClassicTemplate({ resumeItem, watchItem }: Props): React
   );
 
   return (
-    <div className="h-full min-h-[75vh] border p-10 font-sans text-sm md:p-10">
+    <div className="h-full min-h-[75vh] border p-10 font-sans text-xs md:p-10">
       <header className="mb-5">
         <div className="text-lg font-bold">
           <span>{personalDetails?.firstName} </span>
@@ -48,17 +48,17 @@ export default function ClassicTemplate({ resumeItem, watchItem }: Props): React
         <section className="mb-3 font-bold">{summary.description}</section>
 
         <article className="mb-3">
-          <div className="mb-2 text-lg font-bold">Professional Experience</div>
+          <div className="mb-2 text-sm font-bold">Professional Experience</div>
           {workExperiences.map((workExperience, index) => (
             <section key={index} className="mb-3">
               <div className="font-bold">{workExperience.positionTitle}</div>
-              <div className="mb-1 flex justify-between">
-                <div className="italic">
+              <div className="mb-1 flex justify-between italic">
+                <div>
                   <span>{workExperience.companyName}</span>
                   {workExperience.city && <span>, {workExperience.city}</span>}
                   {workExperience.state && <span>, {workExperience.state}</span>}
                 </div>
-                <div className="italic">
+                <div>
                   {formatPeriod(workExperience.startDate, workExperience.endDate, workExperience.currentlyWorkingHere)}
                 </div>
               </div>
@@ -68,21 +68,19 @@ export default function ClassicTemplate({ resumeItem, watchItem }: Props): React
         </article>
 
         <article className="mb-3">
-          <div className="mb-2 text-lg font-bold">Education</div>
+          <div className="mb-2 text-sm font-bold">Education</div>
           {educations.map((education, index) => (
             <section key={index} className="mb-3">
               <div className="font-bold">
                 <span>{education.degree}</span>
                 {education.fieldStudy && <span>, {education.fieldStudy}</span>}
               </div>
-              <div className="mb-1 flex justify-between">
-                <div className="italic">
+              <div className="mb-1 flex justify-between italic">
+                <div>
                   <span>{education.schoolName}</span>
                   {education.schoolLocation && <span>at {education.schoolLocation}</span>}
                 </div>
-                <div className="italic">
-                  {formatPeriod(education.startDate, education.endDate, education.currentlyStudyingHere)}
-                </div>
+                <div>{formatPeriod(education.startDate, education.endDate, education.currentlyStudyingHere)}</div>
               </div>
               {education.educationSummary}
             </section>
@@ -90,7 +88,7 @@ export default function ClassicTemplate({ resumeItem, watchItem }: Props): React
         </article>
 
         <article className="mb-3">
-          <div className="mb-2 text-lg font-bold">Key Skills</div>
+          <div className="mb-2 text-sm font-bold">Key Skills</div>
           <section>
             <ul className="list-disk">
               {skills.items
@@ -107,14 +105,14 @@ export default function ClassicTemplate({ resumeItem, watchItem }: Props): React
 
         {additional?.customSections && additional.customSections.items.length > 0 && (
           <article className="mb-5">
-            <div className="mb-3 text-lg font-bold">{additional.customSections.section}</div>
+            <div className="mb-3 text-sm font-bold">{additional.customSections.section}</div>
             {additional.customSections?.items.map((customSection, index) => (
               <section key={index} className="mb-3">
-                <div className="flex justify-between">
-                  <div className="font-bold">{customSection.name}</div>
-                  <div className="italic">{formatPeriod(customSection.startDate, customSection.endDate)}</div>
+                <div className="font-bold">{customSection.name}</div>
+                <div className="mb-1 flex justify-between italic">
+                  <div>{combineTextWithSeparator(customSection.address1, customSection.address2)}</div>
+                  <div>{formatPeriod(customSection.startDate, customSection.endDate)}</div>
                 </div>
-                <div>{combineTextWithSeparator(customSection.address1, customSection.address2)}</div>
                 <div>{customSection.summary}</div>
               </section>
             ))}
